99fbdd46bec4509f40b1fdff478242df9a23c14d
Vlad <kamlasa@ukr.net>
2021-08-29 18:05:34 +0300

    finish work

--- src/assets/scripts/gulp-modules/infrastructure.js
+++ src/assets/scripts/gulp-modules/infrastructure.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sliders = $('.js-tablet-slider')
+  const sliders = $('.js-tablet-slider');
 
-    $(window).on('resize', () => {
-        if($(window).width() <= 850) {
-            console.log(sliders)
-            sliders.each((_, item) => {
-                const slider = new Swiper(item, {
-                    speed: 700,
-                    autoHeight: true,
-                    freeMode: false,
-                    adaptiveHeight: true,
-                    spaceBetween: 20,
-                    allowTouchMove: true,
-                    slidesPerView: 2.2
-                })
-            })
-        }
-    }).resize()
-})
+  $(window)
+    .on('resize', () => {
+      if ($(window).width() <= 850) {
+        sliders.each((_, item) => {
+          const slider = new Swiper(item, {
+            speed: 700,
+            adaptiveHeight: true,
+            spaceBetween: 20,
+            slidesPerView: 2.3,
+          });
+        });
+      }
+    })
+    .resize();
+});
